c49e7c3110f134ae524ce3096c9399f97ad66cbb
AlejandraJaramillo <alejandracjaramillo@hotmail.com>
2018-11-29 09:39:45 +0100

    add some changes

--- index.js
+++ index.js
@@ -44,29 +44,33 @@ app.post("/registration", (req, res) => {
     //we have to insert first,last,email,hashedpass into the db.js
     //put userId in session
     //send a response
-    hash(req.body.password).then(hash => {
-        console.log("hashedpassword in post /registration:", hash);
-        return db
-            .createUser(
-                req.body.firstname,
-                req.body.lastname,
-                req.body.email,
-                hash
-            )
-            .then(results => {
-                console.log("results in post /registration:", results);
-                req.session.user_id = results.rows[0].id;
-                //console.log(req.session.user_id);
-                req.session.firstname = results.rows[0].firstname;
-                req.session.lastname = results.rows[0].lastname;
-                req.session.email = results.rows[0].email;
-                res.json({success: true});
-            })
+    if (req.body.password != "") {
+        hash(req.body.password).then(hash => {
+            console.log("hashedpassword in post /registration:", hash);
+            return db
+                .createUser(
+                    req.body.firstname,
+                    req.body.lastname,
+                    req.body.email,
+                    hash);
+
+        }).then(results => {
+            console.log("results in post /registration:", results);
+            req.session.user_id = results.rows[0].id;
+            //console.log(req.session.user_id);
+            req.session.firstname = results.rows[0].firstname;
+            req.session.lastname = results.rows[0].lastname;
+            req.session.email = results.rows[0].email;
+            res.json({success: true});
+        })
             .catch(function(error) {
                 console.log("error in post /registration:", error);
                 res.json({success: false});
             });
-    });
+    } else {
+        console.log("please, add a password");
+    }
+
 }); //end post registration
 
 

8923e0e17d1275e28f9d102d9d460aef3b7b1bc4
AlejandraJaramillo <alejandracjaramillo@hotmail.com>
2018-12-14 14:55:07 +0100

    a small change

--- src/profile.js
+++ src/profile.js
@@ -6,7 +6,7 @@ import Bio from "./bio";
 export default function Profile(props) {
     return (
         <div id="profile">
-            <ProfilePic showUploader={props.showUploader} profilePicUrl={props.profilePicUrl} />
+            <ProfilePic showUploader={props.showUploader} profilePicUrl={props.profilePicUrl || "/profile.png"} />
             <br/>
             {props.firstname} {props.lastname}
             <Bio bio={props.bio} setBio={props.setBio} />
